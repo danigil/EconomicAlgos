48da5db96864f6976e1a49ddd4bc7ec31c189625
Daniel Gilkarov <dgilkarov33@gmail.com>
2024-01-30 16:41:15 +0200

    cleaned up comments

--- pareto_eff_netx.py
+++ pareto_eff_netx.py
@@ -5,37 +5,6 @@ import numpy as np
 from copy import deepcopy
 import math
 
-# def get_value(valuations: list[list[float]], agent, item):
-#     return valuations[agent][item]
-
-# def return_pareto_enhancement(valuations: List[List[float]], allocation: List[List[float]]) -> Union[bool, List[List[float]]]:
-#     n_participants = len(valuations)
-#     n_items = len(valuations[0])
-
-#     def is_pareto_efficient(valuations: List[List[float]], allocation: List[List[float]]) -> bool:
-#         def construct_switching_graph(allocation: List[List[float]]) -> nx.DiGraph:
-#             G = nx.DiGraph()
-#             for i, j in itertools.permutations(range(n_participants), 2):
-#                 i_items = set([k for k in range(n_items) if allocation[i][k] > 0])
-#                 ratios = [valuations[i][item]/valuations[j][item] for item in i_items]
-#                 min_ratio = min(ratios)
-
-#                 G.add_edge(i, j, weight=min_ratio)
-
-#             return G
-
-#         G = construct_switching_graph(allocation)
-#         # for edge in G.edges:
-#         #     print(edge, G[edge[0]][edge[1]]['weight'])
-#         cycles = list(nx.simple_cycles(G))
-#         for cycle in cycles:
-#             # print(f'cycle: {cycle}')
-#             # print([(i,j) for i,j in zip(cycle, cycle[1:]+[cycle[0]])])
-#             weights = [G[i][j]['weight'] for i,j in zip(cycle, cycle[1:]+[cycle[0]])]
-#             if np.prod(weights) < 1:
-#                 return False
-#         return True
-
 def is_allocation_valid(allocation: List[List[float]]):
     n_participants = len(allocation)
     assert n_participants > 0
@@ -72,34 +41,23 @@ def is_pareto_efficient(valuations: List[List[float]], allocation: List[List[flo
             ratios = [(valuations[i][item]/valuations[j][item], item) for item in i_items]
             min_ratio = min(ratios, key=lambda x: x[0])
 
-            # G.add_edge(i, j, weight=round(np.log2(min_ratio[0]), 6))
-            # G.add_edge(i, j, weight=np.log2(min_ratio[0]))
             G.add_edge(i, j, weight=min_ratio[0])
             items[(i, j)] = min_ratio[1]
 
         return G, items
 
     G, items = construct_switching_graph(allocation)
-    # for edge in G.edges:
-        # print(edge, G[edge[0]][edge[1]]['weight'])
+
     found=False
     cycles = list(nx.simple_cycles(G))
     for cycle in cycles:
-        # print(f'cycle: {cycle}')
-        # print([(i,j) for i,j in zip(cycle, cycle[1:]+[cycle[0]])])
         weights = [G[i][j]['weight'] for i,j in zip(cycle, cycle[1:]+[cycle[0]])]
         if np.prod(weights) < 1:
-            # print(f'cycle: {cycle}')
-            # print(f'weights: {weights}')
-            # print([(i,j) for i,j in zip(cycle, cycle[1:]+[cycle[0]])])
             found=True
             break
     
     if found:
         cycle += [cycle[0]]
-    # if nx.algorithms.shortest_paths.weighted.negative_edge_cycle(G):
-    #     cycle = nx.algorithms.shortest_paths.weighted.find_negative_cycle(G, 0)
-        # print(f'cycle: {cycle}')
         curr_fraction = fraction
         prev_val=None
 
@@ -120,8 +78,6 @@ def is_pareto_efficient(valuations: List[List[float]], allocation: List[List[flo
         is_alloc_a_pareto_enhancement(valuations, new_allocation, allocation)
 
         return new_allocation
-            # print(f'item: {item}')
-            # print(f'edge: {i, j}, weight: {G[i][j]["weight"]}')
     else:
         return True
     
@@ -150,9 +106,6 @@ def test():
         allocation = [[5/8, 0, 0], [3/8, 1, 1]]
 
         better_allocation = is_pareto_efficient(valuations, allocation)
-        # print(better_allocation)
-
-        # assert is_pareto_efficient(valuations, allocation) != True
 
     def test2():
         valuations = [[3, 1, 6], [6, 3, 1], [1, 6, 3]]
@@ -168,11 +121,9 @@ if __name__ == '__main__':
     allocation = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 
     ret = is_pareto_efficient(valuations, allocation)
-    # print(ret)
 
     allocation = [[1, 0.2, 0], [0, 0.8, 0], [0, 0, 1]]
 
     ret = is_pareto_efficient(valuations, allocation)
-    # print(ret)
 
     test()
